9aacc6ed778ef7d4148dee587e3013c53f587c81
Jarmax <jarmax38@gmail.com>
2021-07-29 01:02:03 +0200

    Formating SD CARD

--- Deployment.py
+++ Deployment.py
@@ -1,6 +1,9 @@
+import os
 import zipfile as zip
 import platform
 import shutil
+import patoolib
+import time
 
 
 def unZipFile(target, path):
@@ -11,5 +14,35 @@ def unZipFile(target, path):
         shutil.rmtree("/Users/maxime/Desktop/__MACOSX")
 
 
+def unRarFile(target, path):
+    patoolib.extract_archive(target, verbosity=0, outdir=path)
+    os.remove(target)
+
+
 def moveFile(name, path1, path2):
     shutil.move(path1 + name, path2 + name)
+
+
+sdCard = ""
+saveRar = ""
+
+with open("C:\\Users\\Dell\\Desktop\\runner\\config.txt", encoding='utf8') as f:
+    lineNumber = 0
+    for line in f:
+        lineNumber += 1
+        if lineNumber == 1:
+            sdCard = line.strip()
+        elif lineNumber == 2:
+            saveRar = line.strip()
+    f.close()
+print("Le système de stockage " + sdCard + " va être former en FAT32")
+print("Le fichier " + saveRar + " va ensuite être copier dedans")
+d1 = input("Clickez sur Y pour lancer le processus et N pour stopper le programme: ")
+
+if d1 == "Y":
+    os.system("format /FS:FAT32 D:")
+    moveFile("save.rar", "C:\\Users\\Dell\\Desktop\\", "D:\\")
+    time.sleep(10)
+    unRarFile("D:\\save.rar", "D:\\")
+elif d1 == "N":
+    pass
